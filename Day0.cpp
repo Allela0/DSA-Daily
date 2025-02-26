d0226832353ce56c06576c8e43580b3003b74023
Abin <abinkbiju2@gmail.com>
2025-02-26 16:08:46 +0530

    Update and rename day0q1.cpp to Day0.cpp

--- Day0.cpp
+++ Day0.cpp
@@ -22,3 +22,32 @@ public:
 // Approach: initialize a vector with double with size of nums then add the numbers in nums to ans through a for loop twice
 //TC: O(n)
 //SC: O(n)
+
+
+
+
+
+// question: https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+//Approach: sort the vector nums and then check if the neighbouring elements are equal or not
+//TC: O(nlogn)
+//SC: O(logn)
